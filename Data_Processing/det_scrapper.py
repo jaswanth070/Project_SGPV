101d1157550a68f48ba1b41cbc8f71401c34bca3
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2023-07-17 13:38:34 +0530

    update

---
+++ Data_Processing/det_scrapper.py
@@ -0,0 +1,70 @@
+from selenium import webdriver  
+import pandas as pd
+import time  
+from selenium.webdriver.common.keys import Keys  
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup as bs
+
+
+pin = 0
+lst = {}
+
+data = {"Pin_No": [],
+        "Name":[],
+}
+
+chrome_options = Options() 
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://sbtetuat.ap.gov.in/APSBTET/results.do")  
+while pin<=198:
+    try:
+        # if pin in [3,33,56,67]:
+        #     pin = pin + 1
+        #     continue
+        
+        lst.clear()
+        driver.find_element('name',"aadhar1").send_keys((Keys.CONTROL, "a"))  
+        driver.find_element('name',"aadhar1").send_keys(str(f"21596-M-{pin:03d}")) 
+
+        s1 = Select(driver.find_element('name','grade2'))
+        s1.select_by_value('1YR')
+
+        time.sleep(1) 
+        # driver.find_element(By.CLASS_NAME,"btn btn-primary")
+        btn = driver.find_elements(By.CLASS_NAME,"btn-primary")
+        btn[0].send_keys(Keys.ENTER)
+        html = driver.page_source
+        soup = bs(html,'html5lib')
+        tags = soup.findAll('td')
+
+        time.sleep(1)
+        # name_std = soup.findAll('font',attrs={'color':'blue'})
+        # # result = soup.findAll('td')
+        # table_c = soup.findAll('td')
+        lst['pin'] = str(tags[0].text)
+        lst['name'] = str(tags[2].text)
+        # print(lst)
+        
+        data['Pin_No'].append(lst['pin'])
+        data['Name'].append(lst['name'])
+
+        # print(data)
+        time.sleep(0.8)
+        btn = driver.find_elements(By.CLASS_NAME,"btn-primary")
+        btn[0].send_keys(Keys.ENTER)
+
+        pin = pin + 1 
+
+    except:
+        df = pd.DataFrame.from_dict(data)
+        df.to_csv(r'cme_1.csv',index=False,header=True)
+        exit()
+
+driver.close()  
+# print(data)
+df = pd.DataFrame.from_dict(data)
+df.to_csv(r'Mechanical/gana_syd_data.csv',index=False,header=True)
